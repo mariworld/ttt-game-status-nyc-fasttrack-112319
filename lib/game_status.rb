07fbd3c75c906d4dc2cc60f7d8846915868595e2
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-24 06:30:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,8 +25,6 @@ def won?(board)
         pos_3 = board[win_index_3]
         if (pos_1 == "X" && pos_2 == "X" && pos_3 == "X") or (pos_1 == "O" && pos_2 == "O" && pos_3 == "O")
             return combo
-        else
-            return false
         end
     end
 end
