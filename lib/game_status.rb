cb9805e46d2e5b263319667c7a97076e5784e281
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-24 06:14:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,11 +20,14 @@ def won?(board)
         win_index_1 = combo[0]
         win_index_2 = combo[1]
         win_index_3 = combo[2]
+
         pos_1 = board[win_index_1]
         pos_2 = board[win_index_2]
         pos_3 = board[win_index_3]
-        if (pos_1 == "X" && pos_2 == "X" && pos_3 == "X") or (pos_1 == "O" && pos_2 == "O" && pos_3 == "O")
+        # if (pos_1 == "X" && pos_2 == "X" && pos_3 == "X") or (pos_1 == "O" && pos_2 == "O" && pos_3 == "O")
+        if combo[0] == combo[1] && combo[1] == combo [2]
             return combo
+        elsif
         else
             return false
         end
